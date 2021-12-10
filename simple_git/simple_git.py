00706391478ad49693ee44187ee0680076d3424a
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-12-10 02:50:57 +0100

    status and get_paths methods have been added.

--- simple_git/simple_git.py
+++ simple_git/simple_git.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import stat
+import time
 
 
 class Simplegit:
@@ -22,4 +24,21 @@ class Simplegit:
 
     @staticmethod
     def status():
-        pass
+        all_paths = Simplegit._get_all_paths()
+        for path in all_paths:
+            print(path, ":", time.ctime(os.stat(path)[ stat.ST_MTIME ]))
+
+    @staticmethod
+    def _get_all_paths():
+        all_paths = []
+        for file_names in os.walk("."):
+            all_paths.extend(Simplegit._get_paths(file_names))
+        return all_paths
+
+    @staticmethod
+    def _get_paths(file_names: tuple) -> list:
+        paths = []
+        for file in file_names[2]:
+            path = file_names[0] + "/" + file
+            paths.append(path[2:])
+        return paths
